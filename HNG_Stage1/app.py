7e6da99e9f1927b1527f1ca9012ee63c9595da41
DavidIfebueme <davidifebueme@gmail.com>
2023-09-07 22:14:25 +0100

    Edits to endpoint and adding requirements.txt

--- HNG_Stage1/app.py
+++ HNG_Stage1/app.py
@@ -1,39 +1,48 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, Response
 import os
 import datetime
+import json
+from collections import OrderedDict
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
-
-def get_infomation():
+def get_information():
     try:
         slack_name = request.args.get('slack_name')
         track = request.args.get('track')
 
         if not slack_name or not track:
-            return jsonify({'error': 'Both slack_name and track are required params', 'status_code': 400}), 400
+            response_data = {'error': 'Both slack_name and track are required params', 'status_code': 400}
+            return Response(json.dumps(response_data, indent=4), content_type='application/json'), 400
 
-        day_of_week = datetime.datetime.now().strftime('%A')
-        current_utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
-        github_mainfile_url = request.args.get('github_mainfile_url')
-        github_repo_url = request.args.get('github_repo_url')
-        status_code = 200 # for success
+        current_day = datetime.datetime.now().strftime('%A')
+        # Get the true current UTC time
+        true_current_utc_time = datetime.datetime.utcnow()
 
-        response_info = {
-            'slack_name': slack_name,
-            'day_of_week': day_of_week,
-            'current_utc_time': current_utc_time,
-            'github_mainfile_url': github_mainfile_url,
-            'github_repo_url': github_repo_url,
-            'status_code': status_code
-        }
+        # Format the current UTC time in ISO 8601 format
+        utc_time = true_current_utc_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-        return jsonify(response_info)
+        github_file_url = 'https://github.com/DavidIfebueme/HNG-Backend-Tasks/blob/master/HNG_Stage1/app.py_mainfile_url'
+        github_repo_url = 'https://github.com/DavidIfebueme/HNG-Backend-Tasks'
+        status_code = 200  # for success
+
+        # Create an ordered dictionary with the desired order of keys
+        response_info = OrderedDict([
+            ('slack_name', slack_name),
+            ('current_day', current_day),
+            ('utc_time', utc_time),
+            ('track', track),
+            ('github_file_url', github_file_url),
+            ('github_repo_url', github_repo_url),
+            ('status_code', status_code)
+        ])
+
+        return Response(json.dumps(response_info, indent=4), content_type='application/json')
 
     except Exception as e:
-        return jsonify({'error': str(e), 'status_code': 500}), 500    
-
+        response_data = {'error': str(e), 'status_code': 500}
+        return Response(json.dumps(response_data, indent=4), content_type='application/json'), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
